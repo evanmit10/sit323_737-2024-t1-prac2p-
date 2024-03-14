da619da4e0a4c710f534c88c47eee59b2ef1a2f3
evanmit10 <94954913+evanmit10@users.noreply.github.com>
2024-03-14 15:39:03 +1100

    complete

--- server_get.js
+++ server_get.js
@@ -1,22 +1,3 @@
-/*const express= require("express");
-const res = require("express/lib/response");
-const app= express();
-const addTwoNumber= (n1,n2) => {
-    return n1+n2;
-}
-app.get("/addTwoNumber", (req,res)=>{
-    const n1= parseInt(req.query.n1);
-    const n2=parseInt(req.query.n2);
-    const result = addTwoNumber(n1,n2);
-    res.json({statuscocde:200, data: result }); 
-});
-app.get("Hello");
-console.log (addTwoNumber(19,12));
-const port=3040;
-app.listen(port,()=> {
-    console.log("hello i'm listening to port "+port);
-})*/
-
 const express= require("express"); //this is part of building an express java webpage after installing express as 'require' is a function for running javascript code on a server and const declares a variable called express.
 const app= express();  //this declares variable called app to link it to variable express to make the webpage work
 const addTwoNumber= (n1,n2) => { //this and the next code below is what the variables n1 and n2 would do in order to make the code work
